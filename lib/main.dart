f5df97d2dfc109af3a20376a9882156aac2a0db4
Mahmoud Abdel Moaz <33752332+Mahmoud-Abdel-Moaz@users.noreply.github.com>
2023-11-16 16:34:37 +0200

    white Labeling

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: 'assets/client.env');
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
 
   runApp(const MyApp());
 }
